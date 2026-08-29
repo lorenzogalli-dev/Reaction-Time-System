33e3cd5c8c36fa46897f6942a5d0b3413ff1723b
lorenzogalli-dev <lorenzo.galli155@gmail.com>
2026-08-29 15:11:59 +0200

    Fix IMU init timing on XIAO Sense (add startup delay before myIMU.begin())

--- Arduino/libraries/Seeed_Arduino_LSM6DS3/examples/HighLevelExample/HighLevelExample.ino
+++ Arduino/libraries/Seeed_Arduino_LSM6DS3/examples/HighLevelExample/HighLevelExample.ino
@@ -35,15 +35,16 @@
 LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
 
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
-    while (!Serial);
-    //Call .begin() to configure the IMUs
-    if (myIMU.begin() != 0) {
-        Serial.println("Device error");
-    } else {
-        Serial.println("Device OK!");
-    }
+  Serial.begin(9600);
+  while (!Serial);
+
+  delay(100);  // <-- aggiungi questa riga
+
+  if (myIMU.begin() != 0) {
+    Serial.println("Device error");
+  } else {
+    Serial.println("Device OK!");
+  }
 }
 
 void loop() {
